4721d3455b8809e1e97974d70bfc804b0258fa1a
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-05 11:45:49 +0100

    style app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
+const messages = [
+  "Learn React ⚛️",
+  "Apply for jobs 💼",
+  "Invest your new income 🤑",
+];
 export default function App() {
-  return <div>hello react</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="active">1</div>
+        <div>2</div>
+        <div>3</div>
+      </div>
+      <p className="message">hello</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }
