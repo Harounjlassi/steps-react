59ef79f7dc3ac5eadf507627fde54b6a0e292d56
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-05 11:50:15 +0100

    fix active steps className

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ const messages = [
   "Invest your new income 🤑",
 ];
 export default function App() {
+  const step = 1;
   return (
     <div className="steps">
       <div className="numbers">
-        <div className="active">1</div>
-        <div>2</div>
-        <div>3</div>
+        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
       </div>
-      <p className="message">hello</p>
+      <p className="message">
+        Step {step}: {messages[step - 1]}
+      </p>
       <div className="buttons">
         <button style={{ backgroundColor: "#7950f2", color: "#ffffff" }}>
           Previous
