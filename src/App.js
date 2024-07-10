77fe9a9003bccf4c6a7d7bdf116a6d279b8ed31c
Harounjlassi <jlassi.haroun@esprit.tn>
2024-07-10 13:35:06 +0100

    add another children prop

--- src/App.js
+++ src/App.js
@@ -48,10 +48,8 @@ function Steps() {
             <div className={`${step >= 2 ? "active" : ""}`}>2</div>
             <div className={`${step >= 3 ? "active" : ""}`}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-            {/*test.name*/}
-          </p>
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
             <Button
               bgColor="#7950f2"
@@ -66,6 +64,14 @@ function Steps() {
           </div>
         </div>
       )}
+    </div>
+  );
+}
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
     </div>
   );
 }
